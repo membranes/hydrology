d2724e5e581c0970a04b4e2d0171349d313035e1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-05-30 21:34:14 +0100

    Add src/main/scala/com/grey/environment/LocalSettings.scala

---
+++ src/main/scala/com/grey/environment/LocalSettings.scala
@@ -0,0 +1,24 @@
+package com.grey.environment
+
+import java.nio.file.Paths
+
+
+/**
+ *
+ */
+class LocalSettings {
+
+  // The environment
+  private val projectDirectory: String = System.getProperty("user.dir")
+  private val separator: String = System.getProperty("file.separator")
+
+  // Data directory
+  val dataDirectory: String = s"$projectDirectory${separator}data"
+
+  // Warehouse directory
+  val warehouseDirectory: String = Paths.get(projectDirectory, "warehouse").toString
+
+  // Resources directory
+  val resourcesDirectory: String = Paths.get(projectDirectory, "src", "main", "resources").toString
+
+}
