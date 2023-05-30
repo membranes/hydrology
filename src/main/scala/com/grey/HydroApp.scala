844da76b65e248f2f5e5b6708f03bae535815eea
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-05-30 21:33:01 +0100

    Update src/main/scala/com/grey/HydroApp.scala

--- src/main/scala/com/grey/HydroApp.scala
+++ src/main/scala/com/grey/HydroApp.scala
@@ -1,5 +1,45 @@
-package   com.grey
+package com.grey
+
+import com.grey.environment.LocalSettings
+import com.typesafe.scalalogging
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
 
 object HydroApp {
+
+  private val localSettings = new LocalSettings()
+
+  def main(args: Array[String]): Unit = {
+
+    // Minimising log print-outs
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("aka").setLevel(Level.OFF)
+
+
+    // Spark Session
+    val spark: SparkSession = SparkSession.builder().appName("architecture")
+      .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+      .config("spark.sql.warehouse.dir", localSettings.warehouseDirectory)
+      .getOrCreate()
+
+
+    // Spark logs
+    spark.sparkContext.setLogLevel("ERROR")
+
+
+    //
+    /* == Configuring for computation ==
+     *
+     * java.lang.Runtime.getRuntime.availableProcessors() deduces the number of machine threads
+     * spark.sql.shuffle.partitions: The number of shuffle partitions for joins & aggregation
+     * spark.default.parallelism: The default number of partitions delivered after a transformation
+     * spark.speculation: If true "performs speculative execution of tasks. This means if one or more tasks are
+     *                             running slowly in a stage, they will be re-launched."
+     */
+    val threads: Int = scala.math.ceil(java.lang.Runtime.getRuntime.availableProcessors()).toInt
+    spark.conf.set("spark.sql.shuffle.partitions", threads.toString)
+    spark.conf.set("spark.default.parallelism", threads.toString)
+
+  }
 
 }
