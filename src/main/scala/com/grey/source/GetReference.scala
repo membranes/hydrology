5cbf138b97d134b4aada4aefa7fc1a99baf13e8a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-05-31 23:49:09 +0100

    Update src/main/scala/com/grey/source/GetReference.scala

--- src/main/scala/com/grey/source/GetReference.scala
+++ src/main/scala/com/grey/source/GetReference.scala
@@ -1,17 +1,22 @@
 package com.grey.source
 
+import com.grey.environment.LocalSettings
 import com.grey.functions.{CaseClassOf, SchemaOf}
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 import org.apache.spark.sql.types.StructType
+import org.apache.commons.io
+import org.apache.commons.io.IOUtils
 
+import java.net.URL
 import scala.util.Try
 import scala.util.control.Exception
 
 class GetReference(spark: SparkSession) {
 
   private val schemaOf = new SchemaOf(spark = spark)
+  val localSettings = new LocalSettings()
 
-  def getReference(url: String, schemaString: String): Dataset[Row] = {
+  def getReference(uri: String, schemaString: String): Dataset[Row] = {
 
     // Get the schema details of the reference file
     val schema: Try[StructType] = schemaOf.schemaOf(schemaString = schemaString)
@@ -19,11 +24,11 @@ class GetReference(spark: SparkSession) {
 
     // Read in the reference file via its schema
     val data: Try[DataFrame] = Exception.allCatch.withTry(
-      spark.read.schema(schema.get)
+      spark.read.schema(schema = schema.get)
         .format("csv")
         .option("header", value = true)
         .option("encoding", "UTF-8")
-        .load(url)
+        .load(uri)
     )
 
 
