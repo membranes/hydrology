045e3a32ef7250ccec7452abd44c07b566e9ff43
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-05-31 22:46:47 +0100

    Update src/main/scala/com/grey/DataSteps.scala

--- src/main/scala/com/grey/DataSteps.scala
+++ src/main/scala/com/grey/DataSteps.scala
@@ -1,12 +1,16 @@
 package com.grey
 
+import com.grey.configurations.EnvironmentAgencyAPI
 import org.apache.spark.sql.SparkSession
 
 class DataSteps(spark: SparkSession) {
 
+  private val environmentAgencyAPI = new EnvironmentAgencyAPI(name = "EnvironmentAgencyAPI")
+
   def dataSteps(): Unit = {
 
-
+    val url = environmentAgencyAPI.environmentAgencyAPI(node = "reference", group = "url", key = "determinands")
+    println(url)
 
   }
 
