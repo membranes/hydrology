4c8630d52e05f99991bba3de4510881ee43f741e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-05-31 19:50:13 +0100

    Add src/main/scala/com/grey/functions/CaseClassOf.scala

---
+++ src/main/scala/com/grey/functions/CaseClassOf.scala
@@ -0,0 +1,24 @@
+package com.grey.functions
+
+import org.apache.spark.sql.types.StructType
+
+object CaseClassOf {
+
+  private val scalaDataType = new ScalaDataType
+
+  def caseClassOf(schema: StructType): String = {
+
+    val definitions: Seq[String] = schema.map { variable =>
+      variable.name + ": " + scalaDataType.scalaDataType(dataTypeOfVariable = variable.dataType)
+    }
+
+    s"""
+       |case class DataClass (
+       |  ${definitions.mkString(",\n")}
+       |)
+       """.stripMargin
+
+  }
+
+
+}
