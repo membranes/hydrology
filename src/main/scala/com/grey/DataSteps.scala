f6e2f6f197fdf51ae26ae16f605607e28343d13a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-05-31 23:49:59 +0100

    Update src/main/scala/com/grey/DataSteps.scala

--- src/main/scala/com/grey/DataSteps.scala
+++ src/main/scala/com/grey/DataSteps.scala
@@ -1,16 +1,25 @@
 package com.grey
 
 import com.grey.configurations.EnvironmentAgencyAPI
-import org.apache.spark.sql.SparkSession
+import com.grey.environment.LocalSettings
+import com.grey.source.GetReference
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+
+import java.nio.file.Paths
 
 class DataSteps(spark: SparkSession) {
 
-  private val environmentAgencyAPI = new EnvironmentAgencyAPI(name = "EnvironmentAgencyAPI")
+  private val environmentAgencyAPI = new EnvironmentAgencyAPI(name = "EnvironmentAgencyAPI.conf")
+  private val getReference = new GetReference(spark = spark)
+  private val localSettings = new LocalSettings()
 
   def dataSteps(): Unit = {
 
-    val url = environmentAgencyAPI.environmentAgencyAPI(node = "reference", group = "url", key = "determinands")
-    println(url)
+    val name = environmentAgencyAPI.environmentAgencyAPI(node = "reference", group = "name", key = "determinands")
+    val uri = Paths.get(localSettings.dataDirectory, "references", name).toString
+
+    val determinands: Dataset[Row] = getReference.getReference(uri = uri, schemaString = "schemaDeterminands.json")
+    determinands.show()
 
   }
 
