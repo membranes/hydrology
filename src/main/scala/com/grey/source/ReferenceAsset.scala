5513c159402bd36b0509fc574bae76e78cc219bf
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-01 19:14:18 +0100

    Update src/main/scala/com/grey/source/ReferenceAsset.scala

--- src/main/scala/com/grey/source/ReferenceAsset.scala
+++ src/main/scala/com/grey/source/ReferenceAsset.scala
@@ -1,24 +1,35 @@
 package com.grey.source
 
-import com.grey.configurations.EnvironmentAgencyAPI
 import com.grey.environment.LocalSettings
 import com.grey.functions.UnloadDocument
-import com.grey.interfaces.EnvironmentAgencyInterface.EnvironmentAgencyCase
+import com.grey.interfaces.GetNode
 
 import java.nio.file.Paths
 
+
+/**
+ * This class will download a specified reference asset from the United Kingdom's Environment Agency
+ * data platform, data application programming interface.
+ */
 class ReferenceAsset {
 
-  private val environmentAgencyAPI = new EnvironmentAgencyAPI(name = "EnvironmentAgency.conf")
   private val localSettings = new LocalSettings()
+  private val unloadDocument = new UnloadDocument()
 
-  def referenceAsset(): Unit = {
+  /**
+   *
+   * @param getNode: An assessor method that would retrieve/provide the parameter values of
+   *                 reference asset.  For example, if the value of <name> is `determinands`
+   *                 it will provide `url`, etc., details of the chemical determinands reference asset.
+   * @param name: The name of the reference asset of interest; which will be downloaded and saved locally.
+   */
+  def referenceAsset(getNode: GetNode, name: String): Unit = {
 
-    // Test
-    val unloadDocument = new UnloadDocument()
-    val urlString = environmentAgencyAPI.environmentAgencyAPI(
-      interface = EnvironmentAgencyCase(node = "reference", group = "url", key = "determinands"))
+    // The get the parameters of the asset in focus
+    val node = getNode.getNode(name = name)
+    val urlString = node.url
 
+    // Unload
     unloadDocument.unloadDocument(urlString = urlString + "?_limit=1000000",
       pathString = Paths.get(localSettings.dataDirectory, urlString.split("/").reverse.head).toString)
 
