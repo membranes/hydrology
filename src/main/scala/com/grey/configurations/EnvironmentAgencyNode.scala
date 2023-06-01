69f671300eb45e908bb751b7004e73b70a563ae0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-01 21:04:22 +0100

    Update src/main/scala/com/grey/configurations/EnvironmentAgencyNode.scala

--- src/main/scala/com/grey/configurations/EnvironmentAgencyNode.scala
+++ src/main/scala/com/grey/configurations/EnvironmentAgencyNode.scala
@@ -2,6 +2,11 @@ package com.grey.configurations
 
 import EnvironmentAgency.Node
 
+/**
+ *
+ * @param nodes: An object consisting environment agency nodes, wherein each node is a set of parameters
+ *               for retrieving the internet/space based data, for storing the data locally, etc.
+ */
 class EnvironmentAgencyNode(nodes: EnvironmentAgency.EnvironmentAgency) {
 
   def environmentAgencyNode(name: String): Node = {
