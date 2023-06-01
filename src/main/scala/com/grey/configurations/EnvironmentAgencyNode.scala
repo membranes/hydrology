a550adc2a11a0d5a6bc3181f015d408114fd8157
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-01 21:00:08 +0100

    Update src/main/scala/com/grey/configurations/EnvironmentAgencyNode.scala

--- src/main/scala/com/grey/configurations/EnvironmentAgencyNode.scala
+++ src/main/scala/com/grey/configurations/EnvironmentAgencyNode.scala
@@ -2,9 +2,9 @@ package com.grey.configurations
 
 import EnvironmentAgency.Node
 
-class GetEnvironmentAgencyNode(nodes: EnvironmentAgency.EnvironmentAgency) {
+class EnvironmentAgencyNode(nodes: EnvironmentAgency.EnvironmentAgency) {
 
-  def getEnvironmentAgencyNode(name: String): Node = {
+  def environmentAgencyNode(name: String): Node = {
 
     val excerpt: List[Node] = nodes.nodes.filter(_.name == name)
 
