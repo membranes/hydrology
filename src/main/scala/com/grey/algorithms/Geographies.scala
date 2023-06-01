aa89562b969694674120654d54b02e1c501902a7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-01 22:43:06 +0100

    Add src/main/scala/com/grey/algorithms/Geographies.scala

---
+++ src/main/scala/com/grey/algorithms/Geographies.scala
@@ -0,0 +1,18 @@
+package com.grey.algorithms
+
+
+/**
+ * https://environment.data.gov.uk/water-quality/id/sampling-point?
+ *    area=3-34&samplingPointType=F6&_limit=1000000
+ *
+ *    --- structure & store (warehouse directory)
+ *
+ */
+class Geographies {
+
+  def geographies(): Unit = {
+
+
+  }
+
+}
