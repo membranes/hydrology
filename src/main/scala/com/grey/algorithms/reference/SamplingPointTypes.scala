46f5738a95e474b8d087e5bad862488661c6ecbe
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-02 22:51:44 +0100

    Update src/main/scala/com/grey/algorithms/reference/SamplingPointTypes.scala

--- src/main/scala/com/grey/algorithms/reference/SamplingPointTypes.scala
+++ src/main/scala/com/grey/algorithms/reference/SamplingPointTypes.scala
@@ -1,5 +1,29 @@
 package com.grey.algorithms.reference
 
-class SamplingPointTypes {
+import org.apache.spark.sql.functions.{col, trim}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+
+class SamplingPointTypes(spark: SparkSession) {
+
+  def samplingPointTypes(reference: Dataset[Row]): Unit = {
+
+    // The names of the relevant columns in the raw data file, and the preferred
+    // names, i.e., <synonyms>
+    val names = Seq("notation", "label", "group", "group.label")
+    val synonyms = Seq("sampling_point_type_id", "sampling_point_type_description", "group", "group_desc")
+
+    // The relevant area columns
+    var data: DataFrame = reference.selectExpr(names: _*)
+
+    // Renaming
+    val fields: Seq[(String, String)] = names.zip(synonyms)
+    fields.foreach { case (name: String, synonym: String) =>
+      data = data.withColumnRenamed(existingName = name, newName = synonym)
+      data = data.withColumn(synonym, trim(col(synonym)))
+    }
+    data.show()
+
+
+  }
 
 }
