1084eaaeef066113ee431fca29a5a20723f5dfd6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-02 18:45:31 +0100

    Update src/main/scala/com/grey/algorithms/Measures.scala

--- src/main/scala/com/grey/algorithms/Measures.scala
+++ src/main/scala/com/grey/algorithms/Measures.scala
@@ -3,7 +3,7 @@ package com.grey.algorithms
 
 /**
  * https://environment.data.gov.uk/water-quality/data/measurement?
- *    area=3-34&samplingPointType=F6&isComplianceSample=false&year=2023&_limit=1000000
+ *    area=3-34&isComplianceSample=false&year=2023&_limit=1000000
  *
  *    --- structure & store (warehouse directory)
  *
