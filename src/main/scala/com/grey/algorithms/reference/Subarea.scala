a18522b5bce6453a322e4c615c24644a0f7cfb69
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-02 22:51:55 +0100

    Update src/main/scala/com/grey/algorithms/reference/Subarea.scala

--- src/main/scala/com/grey/algorithms/reference/Subarea.scala
+++ src/main/scala/com/grey/algorithms/reference/Subarea.scala
@@ -1,16 +1,36 @@
 package com.grey.algorithms.reference
 
 import com.grey.environment.LocalSettings
-import com.grey.source.ReferenceData
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, split, trim}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
 
 class Subarea(spark: SparkSession) {
 
-  private val referenceData = new ReferenceData(spark = spark)
+
   private val localSettings = new LocalSettings()
 
-  def subarea(): Unit = {
+  def subarea(reference: Dataset[Row]): Unit = {
+
+    import spark.implicits._
+
+    // The names of the relevant columns in the raw data file, and the preferred
+    // names, i.e., <synonyms>
+    val names = Seq("notation", "label", "subOrganizationOf")
+    val synonyms = Seq("subarea_id", "description", "url_area_id")
+
+    // The relevant area columns
+    var data: DataFrame = reference.selectExpr(names: _*)
+
+    // Renaming
+    val fields: Seq[(String, String)] = names.zip(synonyms)
+    fields.foreach { case (name: String, synonym: String) =>
+      data = data.withColumnRenamed(existingName = name, newName = synonym)
+      data = data.withColumn(synonym, trim(col(synonym)))
+    }
+
+    val test = data.withColumn(colName = "area_id", col = split($"url_area_id", "/"))
+    test.show()
 
   }
 
