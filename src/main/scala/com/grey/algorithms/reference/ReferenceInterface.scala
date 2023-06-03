c595972836bbf67c769e4a99fbfc9a2ec31216f2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-03 03:45:05 +0100

    Add src/main/scala/com/grey/algorithms/reference/ReferenceInterface.scala

---
+++ src/main/scala/com/grey/algorithms/reference/ReferenceInterface.scala
@@ -0,0 +1,27 @@
+package com.grey.algorithms.reference
+
+import com.grey.configurations.EnvironmentAgency
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+
+class ReferenceInterface(spark: SparkSession) {
+
+  private val area = new Area(spark = spark)
+  private val determinands = new Determinands(spark = spark)
+  private val samplingPoint = new SamplingPoint(spark = spark)
+  private val samplingPointTypes = new SamplingPointTypes(spark = spark)
+  private val subarea = new Subarea(spark = spark)
+
+  def referenceInterface(node: EnvironmentAgency.Node, reference: Dataset[Row]): Unit = {
+
+    node.name match {
+      case "environment-agency-area" => area.area(reference = reference)
+      case "determinands" => determinands.determinands(reference = reference)
+      case "sampling-point" => samplingPoint.samplingPoint(reference = reference)
+      case "sampling-point-types" => samplingPointTypes.samplingPointTypes(reference = reference)
+      case "environment-agency-subarea" => subarea.subarea(reference = reference)
+      case _ => sys.error("Missing")
+    }
+
+  }
+
+}
