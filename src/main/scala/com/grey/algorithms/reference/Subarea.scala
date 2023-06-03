0c1038359fd3d922b3e1904a6ae83c6d2ce26b7b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-03 03:45:27 +0100

    Update src/main/scala/com/grey/algorithms/reference/Subarea.scala

--- src/main/scala/com/grey/algorithms/reference/Subarea.scala
+++ src/main/scala/com/grey/algorithms/reference/Subarea.scala
@@ -1,26 +1,42 @@
 package com.grey.algorithms.reference
 
-import com.grey.environment.LocalSettings
-import org.apache.spark.sql.functions.{col, split, trim}
+import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.functions.{col, trim, udf}
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
 
+/**
+ *
+ * @param spark : An instance of SparkSession
+ */
 class Subarea(spark: SparkSession) {
 
 
-  private val localSettings = new LocalSettings()
-
+  /**
+   * #
+   *
+   * @param reference : The raw data
+   */
   def subarea(reference: Dataset[Row]): Unit = {
 
+    /**
+     * Import implicits for
+     * encoding (https://jaceklaskowski.gitbooks.io/mastering-apache-spark/spark-sql-Encoder.html)
+     * implicit conversions, e.g., converting a RDD to a DataFrames.
+     * access to the "$" notation.
+     */
     import spark.implicits._
+
 
     // The names of the relevant columns in the raw data file, and the preferred
     // names, i.e., <synonyms>
     val names = Seq("notation", "label", "subOrganizationOf")
-    val synonyms = Seq("subarea_id", "description", "url_area_id")
+    val synonyms = Seq("subarea_id", "subarea_desc", "area_id")
+
 
     // The relevant area columns
     var data: DataFrame = reference.selectExpr(names: _*)
+
 
     // Renaming
     val fields: Seq[(String, String)] = names.zip(synonyms)
@@ -29,7 +45,12 @@ class Subarea(spark: SparkSession) {
       data = data.withColumn(synonym, trim(col(synonym)))
     }
 
-    val test = data.withColumn(colName = "area_id", col = split($"url_area_id", "/"))
+
+    // A test
+    val equation: UserDefinedFunction = udf((x: String) => {
+      x.split("/").reverse.head
+    })
+    val test = data.withColumn(colName = "area_id", col = equation($"area_id"))
     test.show()
 
   }
