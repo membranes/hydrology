59c17a5cf58f855050da212f8ea59c18a9abf28e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-03 03:43:29 +0100

    Update src/main/scala/com/grey/algorithms/reference/Determinands.scala

--- src/main/scala/com/grey/algorithms/reference/Determinands.scala
+++ src/main/scala/com/grey/algorithms/reference/Determinands.scala
@@ -3,17 +3,34 @@ package com.grey.algorithms.reference
 import org.apache.spark.sql.functions.{col, trim}
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
+
+/**
+ *
+ * @param spark : An instance of Spark Session
+ */
 class Determinands(spark: SparkSession) {
 
+
+  /**
+   *
+   * @param reference : The <determinands> reference data
+   */
   def determinands(reference: Dataset[Row]): Unit = {
+
+
+    // Addressing the <dot> issue
+    val blob = reference.toDF(reference.columns.map(_.replace(".", "_")): _*)
+
 
     // The names of the relevant columns in the raw data file, and the preferred
     // names, i.e., <synonyms>
-    val names = Seq("notation", "label", "definition", "unit.label", "unit.comment")
+    val names = Seq("notation", "label", "definition", "unit_label", "unit_comment")
     val synonyms = Seq("determinand_id", "determinand_desc", "definition", "unit_of_measure", "unit_of_measure_desc")
 
+
     // The relevant area columns
-    var data: DataFrame = reference.selectExpr(names: _*)
+    var data: DataFrame = blob.selectExpr(names: _*)
+
 
     // Renaming
     val fields: Seq[(String, String)] = names.zip(synonyms)
@@ -22,6 +39,7 @@ class Determinands(spark: SparkSession) {
       data = data.withColumn(synonym, trim(col(synonym)))
     }
     data.show()
+
 
   }
 
