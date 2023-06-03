20b6cfdc11a9c6a60e5d05e6af4d27570d8b3c15
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-03 03:46:42 +0100

    Update src/main/scala/com/grey/DataSteps.scala

--- src/main/scala/com/grey/DataSteps.scala
+++ src/main/scala/com/grey/DataSteps.scala
@@ -1,5 +1,6 @@
 package com.grey
 
+import com.grey.algorithms.reference.ReferenceInterface
 import com.grey.configurations.{EnvironmentAgency, EnvironmentAgencyNode}
 import com.grey.environment.LocalSettings
 import com.grey.source.{ReferenceAsset, ReferenceData}
@@ -11,47 +12,51 @@ import scala.collection.parallel.ParSeq
 
 /**
  *
- * @param spark: A instance of SparkSession
+ * @param spark : A instance of SparkSession
  */
 class DataSteps(spark: SparkSession) {
 
   private val nodes: EnvironmentAgency.EnvironmentAgency = EnvironmentAgency.environmentAgency()
   private val getNode: EnvironmentAgencyNode = new EnvironmentAgencyNode(nodes = nodes)
+  private val referenceInterface = new ReferenceInterface(spark = spark)
 
   private val referenceData = new ReferenceData(spark = spark)
   private val localSettings = new LocalSettings()
-
 
   /**
    *
    */
   def dataSteps(): Unit = {
 
+
     // Nodes
     val names: Seq[String] = List("determinands", "environment-agency-area", "environment-agency-subarea",
       "sampling-point", "sampling-point-types")
-    val nodes: ParSeq[EnvironmentAgency.Node] = names.par.map{ name =>
+    val nodes: ParSeq[EnvironmentAgency.Node] = names.par.map { name =>
       getNode.environmentAgencyNode(name = name)
     }
 
+
     // Downloading the reference assets of interest
-    nodes.par.foreach{node =>
+    nodes.par.foreach { node =>
       // Download the reference data asset
       new ReferenceAsset().referenceAsset(node = node)
     }
 
-    // Previewing
-    nodes.foreach{node =>
 
+    // Structuring
+    nodes.foreach { node =>
       // Read the reference data asset
       val uri = Paths.get(localSettings.referencesDirectory, node.base).toString
       val reference: Dataset[Row] = referenceData.referenceData(uri = uri, schemaString = node.schema)
-
-      // Preview
-      reference.show()
-
+      // Structure
+      referenceInterface.referenceInterface(node = node, reference = reference)
     }
 
   }
 
 }
+
+
+
+
