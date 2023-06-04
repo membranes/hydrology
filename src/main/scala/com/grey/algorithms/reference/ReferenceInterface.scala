c30c745cc907a867175a850aecacec6fc1ae64ca
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-04 23:28:30 +0100

    Update src/main/scala/com/grey/algorithms/reference/ReferenceInterface.scala

--- src/main/scala/com/grey/algorithms/reference/ReferenceInterface.scala
+++ src/main/scala/com/grey/algorithms/reference/ReferenceInterface.scala
@@ -11,7 +11,8 @@ class ReferenceInterface(spark: SparkSession) {
   private val samplingPointTypes = new SamplingPointTypes(spark = spark)
   private val subarea = new Subarea(spark = spark)
 
-  def referenceInterface(node: EnvironmentAgency.Node, reference: Dataset[Row]): Unit = {
+  def referenceInterface(node: EnvironmentAgency.Node, reference: Dataset[Row],
+                         subareaFrame: Dataset[Row] = null, samplingPointTypesFrame: Dataset[Row] = null): Unit = {
 
     node.name match {
       case "environment-agency-area" => area.area(reference = reference)
