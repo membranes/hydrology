b23dea4aa230279710514bfad9b4dbe6b9a0e3f9
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-08 18:07:01 +0100

    Update src/main/scala/com/grey/algorithms/reference/Structuring.scala

--- src/main/scala/com/grey/algorithms/reference/Structuring.scala
+++ src/main/scala/com/grey/algorithms/reference/Structuring.scala
@@ -1,7 +1,11 @@
 package com.grey.algorithms.reference
 
 import com.grey.configurations.EnvironmentAgency
+import com.grey.environment.LocalSettings
+import com.grey.source.ReferenceData
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
+
+import java.nio.file.Paths
 
 class Structuring(spark: SparkSession) {
 
@@ -11,7 +15,21 @@ class Structuring(spark: SparkSession) {
   private val samplingPointTypes = new SamplingPointTypes(spark = spark)
   private val subarea = new Subarea(spark = spark)
 
-  def structuringInitial(node: EnvironmentAgency.Node, reference: Dataset[Row]): Dataset[Row] = {
+  private val referenceData = new ReferenceData(spark = spark)
+  private val localSettings = new LocalSettings()
+
+  private def structuringReference(node: EnvironmentAgency.Node): Dataset[Row] = {
+
+    // Read the reference data asset
+    val uri = Paths.get(localSettings.referencesDirectory, node.base).toString
+    val reference: Dataset[Row] = referenceData.referenceData(uri = uri, schemaString = node.schema)
+    reference
+
+  }
+
+  def structuringInitial(node: EnvironmentAgency.Node): Dataset[Row] = {
+
+    val reference = structuringReference(node = node)
 
     node.name match {
       case "sampling-point-types" => samplingPointTypes.samplingPointTypes(reference = reference)
@@ -21,8 +39,10 @@ class Structuring(spark: SparkSession) {
 
   }
 
-  def structuringMiscellaneous(node: EnvironmentAgency.Node, reference: Dataset[Row],
-                               subareaFrame: Dataset[Row] = null, samplingPointTypesFrame: Dataset[Row] = null): Unit = {
+  def structuringMiscellaneous(node: EnvironmentAgency.Node, subareaFrame: Dataset[Row] = null,
+                               samplingPointTypesFrame: Dataset[Row] = null): Unit = {
+
+    val reference = structuringReference(node = node)
 
     node.name match {
       case "environment-agency-area" => area.area(reference = reference)
